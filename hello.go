38a6e68d32db76369a7a0c437647754408925213
Kanad4s <n.tropin@g.nsu.ru>
2024-08-28 18:20:35 +0700

    complex condition

--- hello.go
+++ hello.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go")
+	for i := 0; i < 7; i++ {
+		if i%2 == 0 {
+			fmt.Println("Hello, Goб чет")
+		} else if i%3 == 0 && (i > 3 || i%2 == 1) {
+			fmt.Println("Hello, Go, нечет, комплексное условие")
+		}
+	}
 }
