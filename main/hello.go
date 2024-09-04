335c38df05b873b75db576a410bf5c0f238d013d
Kanad4s <n.tropin@g.nsu.ru>
2024-09-04 12:24:30 +0700

    add consts

--- main/hello.go
+++ main/hello.go
@@ -2,11 +2,20 @@ package main
 
 import "fmt"
 
+const (
+	i         = 5
+	f         = 1.5
+	i64 int64 = 64
+)
+
+var vTest = 1
+
 func div(a, b int) (int, error) {
 	return a / b, nil
 }
 
 func main() {
+	fmt.Println(f+float64(vTest), i64+int64(vTest))
 	for i := 0; i < 7; i++ {
 		if i%2 == 0 {
 			fmt.Println("Hello, Goб чет")
