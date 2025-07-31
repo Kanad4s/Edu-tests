116c9f1f46c777a7290d468d0b56b8afd7180e80
Kanad4s <n.tropin@g.nsu.ru>
2025-08-01 00:52:56 +0700

    add hill valley

--- go/tests/test.go
+++ go/tests/test.go
@@ -1,77 +1,25 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 )
 
-// Node represents a computation step: either Mul or Sum.
-type Node struct {
-	ID    string   `json:"id"`
-	Op    string   `json:"op"`   // Mul or Sum
-	Args  []int    `json:"args"` // [i, k, j] or [i, j, k]
-	Edges []string `json:"edges"`
+func main() {
+	a := []int{6, 6, 5, 5, 4, 1}
+	// a := []int{6, 17, 5, 38, 19, 94, 44, 47, 10, 70, 41, 90, 65, 100, 87, 95, 32, 36, 76, 95, 61, 18, 60, 28, 22, 27, 52, 16, 19, 24, 47, 10, 28, 35, 56, 40, 81, 83, 88, 67, 66, 89, 8, 88, 16, 16, 44, 95}
+	fmt.Println(countHillValley(a))
 }
 
-func main() {
-	var nodes []Node
-
-	AHeight := 2
-	AWidth := 3
-	BWidth := 6
-
-	// Step 1: Create Mul nodes
-	for i := 0; i < AHeight; i++ {
-		for j := 0; j < BWidth; j++ {
-			for k := 0; k < AWidth; k++ {
-				id := fmt.Sprintf("Mul_%d_%d_%d", i, k, j)
-				nodes = append(nodes, Node{
-					ID:   id,
-					Op:   "Mul",
-					Args: []int{i, k, j},
-				})
+func countHillValley(nums []int) int {
+	count := 0
+	left := 0
+	for i := 1; i < len(nums)-1; i++ {
+		if nums[i] != nums[i+1] {
+			if (nums[i] > nums[left] && nums[i] > nums[i+1]) || (nums[i] < nums[left] && nums[i] < nums[i+1]) {
+				count++
 			}
+			left = i
 		}
 	}
-
-	// Step 2: Create Sum nodes with dependencies
-	for i := 0; i < AHeight; i++ {
-		for j := 0; j < BWidth; j++ {
-			for k := 0; k < AWidth; k++ {
-				id := fmt.Sprintf("Sum_%d_%d_%d", i, j, k)
-				edges := []string{}
-				if k == 0 {
-					edges = append(edges, fmt.Sprintf("Mul_%d_%d_%d", i, k, j))
-				} else {
-					edges = append(edges,
-						fmt.Sprintf("Sum_%d_%d_%d", i, j, k-1),
-						fmt.Sprintf("Mul_%d_%d_%d", i, k, j),
-					)
-				}
-				nodes = append(nodes, Node{
-					ID:    id,
-					Op:    "Sum",
-					Args:  []int{i, j, k},
-					Edges: edges,
-				})
-			}
-		}
-	}
-
-	// Step 3: Export to JSON
-	file, err := os.Create("execution_plan.json")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-	err = encoder.Encode(nodes)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Execution plan exported to execution_plan.json")
+	return count
 }
