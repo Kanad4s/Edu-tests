90ded7b27a3c79b6ea26655a8200794d64ea9654
Kanad4s <n.tropin@g.nsu.ru>
2025-08-04 11:21:38 +0700

    add solution leetcode 2561

--- go/tests/test.go
+++ go/tests/test.go
@@ -2,23 +2,88 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 )
 
 func main() {
-	a := []int{6, 6, 5, 5, 4, 1}
-	// a := []int{6, 17, 5, 38, 19, 94, 44, 47, 10, 70, 41, 90, 65, 100, 87, 95, 32, 36, 76, 95, 61, 18, 60, 28, 22, 27, 52, 16, 19, 24, 47, 10, 28, 35, 56, 40, 81, 83, 88, 67, 66, 89, 8, 88, 16, 16, 44, 95}
-	fmt.Println(majorityElement(a))
+	a := []int{4, 4}
+	b := []int{3, 3}
+	fmt.Println(minCost1(a, b))
 }
-func majorityElement(nums []int) int {
-	count := make(map[int]int)
-	maxNum := 0
-	maxCount := 0
-	for _, num := range nums {
-		count[num] += 1
-		if count[num] > maxCount {
-			maxCount = count[num]
-			maxNum = num
+
+func minCost1(basket1 []int, basket2 []int) int64 {
+	b1 := make(map[int]int)
+	b2 := make(map[int]int)
+	var cost int
+	for _, fruit := range basket1 {
+		b1[fruit]++
+	}
+	for _, fruit := range basket2 {
+		b2[fruit]++
+	}
+	for k, v := range b1 {
+		if (v+b2[k])%2 == 1 {
+			return -1
+		}
+		cost += abs(v-b2[k]) / 2
+	}
+	for k, v := range b2 {
+		_, ok := b1[k]
+		if ok {
+			continue
+		}
+		if v%2 == 1 {
+			return -1
+		}
+		cost += v / 2
+	}
+	return int64(cost)
+}
+
+func minCost2(A, B []int) int64 {
+	freq := map[int]int{}
+	minVal := math.MaxInt
+	for _, x := range A {
+		freq[x]++
+		if x < minVal {
+			minVal = x
 		}
 	}
-	return maxNum
+	for _, x := range B {
+		freq[x]--
+		if x < minVal {
+			minVal = x
+		}
+	}
+
+	extra := []int{}
+	for k, v := range freq {
+		if v%2 != 0 {
+			return -1
+		}
+		for i := 0; i < abs(v)/2; i++ {
+			extra = append(extra, k)
+		}
+	}
+
+	sort.Ints(extra)
+	var cost int64
+	for i := 0; i < len(extra)/2; i++ {
+		cost += int64(min(extra[i], 2*minVal))
+	}
+	return cost
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
