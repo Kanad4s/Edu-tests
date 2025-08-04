05996d209915ad0a8e8e929c466251c0efffbdd2
Kanad4s <n.tropin@g.nsu.ru>
2025-08-04 21:13:26 +0700

    add solution leetcode 1957

--- go/tests/test.go
+++ go/tests/test.go
@@ -2,58 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	// a := []int{4, 2, 2, 4, 3, 3, 3, 3}
-	b := []int{5, 9, 9, 0, 9, 6, 9, 6, 9, 9, 9}
-	fmt.Println(totalFruit(b))
+	// b := []int{5, 9, 9, 0, 9, 6, 9, 6, 9, 9, 9}
+	fmt.Println(makeFancyString("aabbbbrrreweewwww"))
 }
-func totalFruit(fruits []int) int {
-	if len(fruits) <= 2 {
-		return len(fruits)
-	}
-	busketCountA, busketCountB := 1, 0
-	busketTypeA, busketTypeB := fruits[0], 0
-	maxCount := 0
-	curCount := 1
-	for i := 1; i < len(fruits); i++ {
-		if fruits[i] != busketTypeA && fruits[i] != busketTypeB {
-			if maxCount < curCount {
-				maxCount = curCount
-			}
-			if fruits[i-1] == busketTypeA {
-				curCount = busketCountA
-				busketTypeB = fruits[i]
-				busketCountB = 1
-				busketCountA = 0
-			} else if fruits[i-1] == busketTypeB {
-				curCount = busketCountB
-				busketTypeA = fruits[i]
-				busketCountA = 1
-				busketCountB = 0
-			}
-		} else if fruits[i] != busketTypeA || fruits[i] != busketTypeB {
-			if fruits[i] == busketTypeB {
-				busketCountA = 0
-				busketCountB++
-			} else {
-				busketCountB = 0
-				busketCountA++
-			}
+
+func makeFancyString(s string) string {
+	res := strings.Builder{}
+	var letter rune
+	count := 0
+	for _, cur := range s {
+		if cur == letter {
+			count++
 		} else {
-			if fruits[i] == busketTypeA {
-				busketCountA++
-			} else if fruits[i] == busketTypeB {
-				busketCountB++
-			}
+			letter = cur
+			count = 0
 		}
-		curCount++
-		fmt.Printf("curCount: %d, typeA %d, CountA %d, typeB %d, countB: %d\n",
-			curCount, busketTypeA, busketCountA, busketTypeB, busketCountB)
+		if count < 2 {
+			res.WriteRune(cur)
+		}
 	}
-	if curCount > maxCount {
-		maxCount = curCount
-	}
-	return maxCount
+	return res.String()
 }
